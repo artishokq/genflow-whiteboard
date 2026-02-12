3492549e1357ded9ed38d140962f069cc5cf6705
artishokq <astkachuk_2@edu.hse.ru>
2026-02-12 19:19:46 +0300

    updated: обновлен роутинг

--- apps/web/src/app/RouterBuilder.tsx
+++ apps/web/src/app/RouterBuilder.tsx
@@ -1,11 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import LoginPage from "../pages/LoginPage/LoginPage";
+import RecoverPage from "../pages/RecoverPage/RecoverPage";
+import RegisterPage from "../pages/RegisterPage/RegisterPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LoginPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/recover",
+    element: <RecoverPage />,
   },
 ]);
 
